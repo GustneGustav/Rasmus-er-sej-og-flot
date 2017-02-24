6d026dab9cb50e423ef54190c2a269d99921adfc
GustneGustav <hicrasmus@hotmail.com>
2017-02-24 13:37:07 +0100

    Update sketch.js

--- Video/sketch.js
+++ Video/sketch.js
@@ -1,9 +1,11 @@
 var gameover;
 function setup() {
 createCanvas(820, 600); 
-
+gameover=createVideo('video/gameover.webm');
+    gameover.loop();
+    gameover.hide(); 
 }
 function draw() {
 background(50); 
-
+image(gameover, 10, 10); 
 }
